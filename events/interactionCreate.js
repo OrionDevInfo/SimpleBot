8020bb1dc120ae443be8c15c9efd8068a93dfd80
OrionDevInfo <orion.maubert@gmail.com>
2023-03-05 22:46:04 +0100

    Structure

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,14 +8,14 @@ module.exports = {
         const command = interaction.client.cmds.get(interaction.commandName);
 
         if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found for ${client.user.tag}.`);
+            console.log('\x1b[31m%s\x1b[0m', `No command matching ${interaction.commandName} was found for ${client.user.tag}.`);
             return;
         }
 
         try {
             await command.execute(interaction);
         } catch (error) {
-            console.error(`Error executing ${interaction.commandName} for ${client.user.tag}.`);
+            console.log('\x1b[31m%s\x1b[0m', `Error executing ${interaction.commandName} for ${client.user.tag}.`);
             console.error(error);
         }
     },
--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log('\x1b[32m%s\x1b[0m', `[READY] Logged in as ${client.user.tag}`);
+        console.log('\x1b[32m%s\x1b[0m', `[READY] Logged in as ${client.user.tag}.`);
     },
 };
